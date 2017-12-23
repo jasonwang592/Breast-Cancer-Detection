aae3026e00a861401908656dcca8eb41eda0975d
Jason Wang <jason.wang@ARISETARGETOLD.workdayinternal.com>
2017-12-22 17:03:09 -0800

    plotters done, pca next

---
+++ plotters.py
@@ -0,0 +1,120 @@
+import seaborn as sns
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import sys
+
+def pairplots(df, features, output_dir, save = True):
+  '''Plots pairplots for the data frame and desired features to visualize.
+
+  Args:
+    - df        (DataFrame): The dataframe all data (malignant and benign)
+    - features  (list)     : List of feature names that will be pairplotted
+    - output_dir(String)   : The directory to save output to for bivariate_kde plots
+    - save      (Boolean)  : Saves the file by default, if set to False, displays the plot instead
+  '''
+
+  temp = df.loc[:, cols]
+  fname = ['pairplot']
+  for i in cols:
+    if i != 'diagnosis':
+      fname.append(i)
+  sns.pairplot(data = temp, hue = 'diagnosis', palette = 'husl', diag_kind = 'kde')
+
+  if save:
+    if not os.path.exists(output_dir):
+      os.makedirs(output_dir)
+    plt.savefig(output_dir + '_'.join([feature1, feature2]))
+  else:
+    plt.show()
+  plt.close()
+
+def bivariate_kde(df, feature1, feature2, output_dir, save = True):
+  '''Plots the bivariate density plot of two features for malignant and
+  benign breast cancer tumors.
+
+  Args:
+    - df        (DataFrame): The dataframe all data (malignant and benign)
+    - feature1  (String)   : The first feature/metric to be compared
+    - feature2  (String)   : The second feature/metric to be compared
+    - output_dir(String)   : The directory to save output to for bivariate_kde plots
+    - save      (Boolean)  : Saves the file by default, if set to False, displays the plot instead
+
+  Raises:
+    KeyError: If either feature1 or feature2 is not a valid column, raise an error.
+  '''
+  try:
+    sns.set_style('darkgrid')
+    m = df.loc[df['diagnosis'] == 'malignant']
+    b = df.loc[df['diagnosis'] == 'benign']
+
+    f, ax = plt.subplots(figsize = (12, 8))
+    ax = sns.kdeplot(m[feature1], m[feature2], cmap = 'Reds', shade = True, shade_lowest = False)
+    ax = sns.kdeplot(b[feature1], b[feature2], cmap = 'Blues', shade = True, shade_lowest = False)
+  except KeyError as e:
+    raise KeyError('Please provide a valid column feature to plot.')
+
+  title = '\n'.join(['Disbribution of features in benign and malignant breast masses', feature1 + ' vs ' + feature2])
+
+  red = sns.color_palette('Reds')[-2]
+  blue = sns.color_palette('Blues')[-2]
+  ax.text(m[feature1].mean() - 1.5 * m[feature1].std(), m[feature2].mean(), 'malignant', size = 16, color = red)
+  ax.text(b[feature1].mean() - 2 * b[feature1].std(), b[feature2].mean(), 'benign', size = 16, color = blue)
+  plt.title(title)
+  plt.tight_layout()
+
+  if save:
+    if not os.path.exists(output_dir):
+      os.makedirs(output_dir)
+    plt.savefig(output_dir + '_'.join([feature1, feature2]))
+  else:
+    plt.show()
+  plt.close()
+
+def violinplotter(df, output_dir, save = True):
+  '''Helper function to make pairwise violinplots with split turned on to quickly compare high level distributions
+  of features in breast masses examined.
+
+  Args:
+    - df        (DataFrame): The dataframe all data (malignant and benign)
+    - output_dir(String)   : The directory to save output to for violinplots
+    - save      (Boolean)  : Saves the file by default, if set to False, displays the plot instead
+
+  Raises:
+    IndexError: Rasied if the DataFrame provided is not the full DataFrame of the dataset with all columns
+  '''
+
+  #Too many columns so we'll explore by splitting plots
+  chunks = [(1,10), (11,20), (21,30)]
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+  for i, chunk in enumerate(chunks):
+    fname = '_'.join(['violinplot', str(i)])
+    try:
+      x_sub = df.iloc[:, chunk[0]:chunk[1]]
+      x_sub = pd.concat([labs, x_sub], axis = 1)
+      melted = pd.melt(x_sub,
+        id_vars = 'diagnosis',
+        var_name = 'feature',
+        value_name = 'value')
+      sns.set_style('whitegrid')
+      fig = plt.figure(figsize = (15, 9))
+      g = sns.violinplot(x = 'feature', y = 'value', hue = 'diagnosis', data = melted, palette = 'PRGn', split = True)
+      g.set_xticklabels(g.get_xticklabels(), rotation = 45)
+      plt.title('Feature distribution of malignant and benign breast masses')
+      plt.tight_layout()
+    except IndexError as e:
+      raise IndexError('Please provide the full data frame with all 30 features/columns.')
+
+    if save:
+      if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+      plt.savefig(output_dir + fname)
+    else:
+      plt.show()
+    plt.close()
+
+
+
+
+
--- visualize.py
+++ visualize.py
@@ -3,6 +3,8 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import sys
+import plotters
+
 
 output = 'output/'
 if not os.path.exists(output):
@@ -11,9 +13,10 @@ if not os.path.exists(output):
 df = pd.read_csv('files/Breast Cancer.csv')
 diag_map = {'B': 'benign', 'M': 'malignant'}
 df['diagnosis'] = df['diagnosis'].map(diag_map)
+df.drop(['Unnamed: 32', 'id'], axis = 1, inplace = True)
 labs = df['diagnosis']
 x = df.copy(deep = True)
-x.drop(['Unnamed: 32', 'diagnosis', 'id'], axis = 1, inplace = True)
+x.drop(['diagnosis'], axis = 1, inplace = True)
 
 #split train and test data
 train = df.sample(frac = 0.8, random_state = 123)
@@ -22,42 +25,22 @@ test = df.loc[~df.index.isin(train.index)]
 #standardize the columns
 x = (x - x.mean()) / x.std()
 
-#Too many columns so we'll explore by splitting plots
-chunks = [(1,10), (11,20), (21,30)]
+#Plot violinplots for a high level view of each feature
 output_dir = output + 'violinplots/'
-if not os.path.exists(output_dir):
-  os.makedirs(output_dir)
-for i, chunk in enumerate(chunks):
-  fname = '_'.join(['violinplot', str(i)])
-  x_sub = x.iloc[:, chunk[0]:chunk[1]]
-  x_sub = pd.concat([labs, x_sub], axis = 1)
-  melted = pd.melt(x_sub,
-    id_vars = 'diagnosis',
-    var_name = 'feature',
-    value_name = 'value')
-  sns.set_style('whitegrid')
-  fig = plt.figure(figsize = (15, 9))
-  g = sns.violinplot(x = 'feature', y = 'value', hue = 'diagnosis', data = melted, palette = 'PRGn', split = True)
-  g.set_xticklabels(g.get_xticklabels(), rotation = 45)
-  plt.title('Feature distribution of malignant and benign breast cancer tumors')
-  plt.tight_layout()
-  plt.savefig(output_dir + fname)
-  plt.close()
+plotters.violinplotter(x, output_dir)
 
+#Choose features of interest and plot pairplots
+output_dir = output + 'pairwiseplots/'
+cols = ['diagnosis', 'concavity_mean', 'concave points_mean', 'concave points_worst']
+plotters.pairplots(df, cols, output_dir)
+
+# print(df.corr())
+# sns.clustermap(df.corr())
+# plt.tight_layout()
+# plt.show()
+# sys.exit()
 #Explore some bivariate KDE plots for like features
-sns.set_style('darkgrid')
-m = df.loc[df['diagnosis'] == 'malignant']
-b = df.loc[df['diagnosis'] == 'benign']
+# plotters.bivariate_kde(df, 'concavity_mean', 'concave points_mean', output + 'kde_density_plots/', save = True)
 
-f, ax = plt.subplots(figsize = (12, 8))
-
-ax = sns.kdeplot(m['concavity_mean'], m['concave points_mean'], cmap = 'Reds', shade = True, shade_lowest = False)
-ax = sns.kdeplot(b['concavity_mean'], b['concave points_mean'], cmap = 'Blues', shade = True, shade_lowest = False)
-
-red = sns.color_palette('Reds')[-2]
-blue = sns.color_palette('Blues')[-2]
-ax.text(3.8, 4.5, 'malignant', size = 16, color = red)
-ax.text(2.5, 8.2, 'benign', size = 16, color = blue)
-plt.show()
 
 
