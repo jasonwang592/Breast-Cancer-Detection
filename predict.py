648f9ae9846839767a0be04738a7f02092e4a9f1
Jason Wang <jason.wang@ARISETARGETOLD.workdayinternal.com>
2018-01-04 23:20:39 -0800

    wrapping everything up

--- predict.py
+++ predict.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import os
 import subprocess
-import sys
 from sklearn.model_selection import KFold
 from sklearn import tree
 from sklearn.externals.six import StringIO
 from IPython.display import Image
 from sklearn.tree import export_graphviz
 import pydotplus
-import time
 
 output = 'output/'
 if not os.path.exists(output):
@@ -24,16 +22,17 @@ df.drop(['Unnamed: 32', 'id', 'diagnosis'], axis = 1, inplace = True)
 output_dir = output + 'decision_trees/'
 if not os.path.exists(output_dir):
   os.makedirs(output_dir)
-kf = KFold(n_splits = 5, shuffle = True, random_state = 211)
+kf = KFold(n_splits = 5, shuffle = True, random_state = 42)
 fold_accuracy = []
 for train_indices, test_indices in kf.split(df):
   X_train, X_test = df.iloc[train_indices], df.iloc[test_indices]
   Y_train, Y_test = labs[train_indices], labs[test_indices]
 
-  tree_model = tree.DecisionTreeClassifier()
+  tree_model = tree.DecisionTreeClassifier(random_state = 42)
   tree_model.fit(X_train, Y_train)
   preds = tree_model.predict(X_test)
-  print(' '.join(['Fold', str(len(fold_accuracy) + 1), 'Accuracy:', str(sum(preds == Y_test)/len(Y_test))]))
+  accuracy = round((sum(preds == Y_test)/len(Y_test)) * 100, 3)
+  print(' '.join(['Fold', str(len(fold_accuracy) + 1), 'Accuracy:', str(accuracy) + '%']))
 
   fname = ' '.join(['Decision Tree Fold', str(len(fold_accuracy) + 1)])
   with (open(output_dir + fname + '.dot', 'w')) as f:
@@ -45,3 +44,4 @@ for train_indices, test_indices in kf.split(df):
   subprocess.check_call(command)
   os.remove(output_dir + fname + '.dot')
   fold_accuracy.append(sum(preds == Y_test)/len(Y_test))
+print(sum(fold_accuracy)/len(fold_accuracy))
--- visualize.py
+++ visualize.py
@@ -20,6 +20,7 @@ x.drop(['diagnosis'], axis = 1, inplace = True)
 
 #standardize the columns
 x = (x - x.mean()) / x.std()
+y = x.copy(deep = True)
 
 #Plot violinplots for a high level view of each feature
 output_dir = output + 'violinplots/'
@@ -28,10 +29,10 @@ plotters.violinplotter(x, labs,  output_dir)
 #Choose features of interest and plot pairplots
 output_dir = output + 'pairwiseplots/'
 cols = ['diagnosis', 'symmetry_mean', 'fractal_dimension_mean']
-plotters.pairplots(df, cols, output_dir)
+# plotters.pairplots(df, cols, output_dir)
 
 #Explore some bivariate KDE plots for like features
-plotters.bivariate_kde(df, 'concavity_mean', 'concave points_mean', output + 'kde_density_plots/')
+# plotters.bivariate_kde(df, 'concavity_mean', 'concave points_mean', output + 'kde_density_plots/')
 
 output_dir = output + 'clustermaps/'
 if not os.path.exists(output_dir):
@@ -40,7 +41,7 @@ if not os.path.exists(output_dir):
 g = sns.clustermap(x.corr())
 plt.setp(g.ax_heatmap.get_yticklabels(), rotation = 0)
 plt.setp(g.ax_heatmap.get_xticklabels(), rotation = 90)
-plt.savefig(output_dir + 'clustermap_original') #TODO: Figure out how to fix cutoff on this shiznit
+plt.savefig(output_dir + 'clustermap_original', bbox_inches='tight')
 plt.close()
 
 x.drop(['perimeter_mean', 'radius_mean', 'perimeter_worst','radius_worst',
@@ -48,7 +49,7 @@ x.drop(['perimeter_mean', 'radius_mean', 'perimeter_worst','radius_worst',
 g = sns.clustermap(x.corr())
 plt.setp(g.ax_heatmap.get_yticklabels(), rotation = 0)
 plt.setp(g.ax_heatmap.get_xticklabels(), rotation = 90)
-plt.savefig(output_dir + 'clustermap_drop1') #TODO: Figure out how to fix cutoff on this shiznit
+plt.savefig(output_dir + 'clustermap_drop1', bbox_inches='tight')
 plt.close()
 
 x.drop(['compactness_se', 'concavity_se', 'concave points_se','symmetry_se',
@@ -56,18 +57,18 @@ x.drop(['compactness_se', 'concavity_se', 'concave points_se','symmetry_se',
 g = sns.clustermap(x.corr())
 plt.setp(g.ax_heatmap.get_yticklabels(), rotation = 0)
 plt.setp(g.ax_heatmap.get_xticklabels(), rotation = 90)
-plt.savefig(output_dir + 'clustermap_drop2') #TODO: Figure out how to fix cutoff on this shiznit
+plt.savefig(output_dir + 'clustermap_drop2', bbox_inches='tight')
 plt.close()
 
 #Preliminary PCA
-plotters.cum_variance_explained(x, output + 'PCA/')
+plotters.cum_variance_explained(y, output + 'PCA/')
 
 #PCA analysis post basic feature engineering
 plotters.cum_variance_explained(x, output + 'PCA/')
 
-pca = PCA(n_components = x.shape[1])
-pca.fit(x)
-x1 = pca.fit_transform(x)
+pca = PCA(n_components = y.shape[1])
+pca.fit(y)
+x1 = pca.fit_transform(y)
 x1 = pd.DataFrame(x1)
 x1 = pd.concat([labs, x1], axis = 1)
 
@@ -75,12 +76,12 @@ for label, color in zip(('benign', 'malignant'), ('blue','red')):
   plt.scatter(x1[x1['diagnosis'] == label][0], x1[x1['diagnosis'] == label][1], label = label, c = color)
 plt.savefig(output + 'PCA/' + 'Scatter, 2 components')
 
-df = pd.DataFrame(pca.components_, columns=list(x.columns))
+df = pd.DataFrame(pca.components_, columns=list(y.columns))
 
 #Code to generate HTML table to put in blog post because Pandas is stupid
 #and can't print tables in a nice way
 header = []
-ncomponents = 4
+ncomponents = 5
 for i in range(ncomponents):
   header.append('PC' + str(i + 1))
 print(tabulate(df.T.iloc[:, 0:ncomponents], headers = header, tablefmt = 'html'))
